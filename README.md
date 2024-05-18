da10dbd494946170e5034e6676525b26afc36f5a
sjimenezx <35488502+sjimenezx@users.noreply.github.com>
2024-05-18 10:42:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pruebas_angular
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sjimenezx/pruebas_angular)
---
+++ src/example/expansion-expand-collapse-all-example.ts
@@ -0,0 +1,35 @@
+import {Component, ViewChild} from '@angular/core';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
+/**
+ * @title Accordion with expand/collapse all toggles
+ */
+@Component({
+  selector: 'expansion-expand-collapse-all-example',
+  templateUrl: 'expansion-expand-collapse-all-example.html',
+  styleUrl: 'expansion-expand-collapse-all-example.css',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+  ],
+})
+export class ExpansionExpandCollapseAllExample {
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+}
+
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
